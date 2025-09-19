56e0da6eecf9d172f4d2d4b023cbe597fd6ae1f2
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 23:19:05 +0000

    got changed file

--- index.ts
+++ index.ts
@@ -4,7 +4,4 @@ export const multiply = (a: number, b: number): number => a * b;
 export const divide = (a: number, b: number): number => a / b;
 export const exponentiate = (a: number, b: number): number => a ** b;
 export const square = (a: number): number => a * a;
-<<<<<<< HEAD
-=======
 export const cube = (a: number): number => a * a * a;
->>>>>>> refs/subrepo/dist/fetch
