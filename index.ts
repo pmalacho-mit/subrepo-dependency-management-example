13e68e458cf840b8c788473d9567678e0da0b15d
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 21:44:29 +0000

    code trigger

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export const add = (a: number, b: number): number => a + b;
 export const subtract = (a: number, b: number): number => a - b;
 export const multiply = (a: number, b: number): number => a * b;
 export const divide = (a: number, b: number): number => a / b;
+export const exponentiate = (a: number, b: number): number => a ** b;
