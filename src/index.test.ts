08477b3363f630dd68d935df01495f3838d09064
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 19:49:49 +0000

    adding subtract to trigger workflow

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
-import { add } from "./index";
+import { add, subtract } from "./index";
 import { expect, test } from "vitest";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test("subtracts 5 - 2 to equal 3", () => {
+  expect(subtract(5, 2)).toBe(3);
 });
