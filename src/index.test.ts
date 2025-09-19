a26f357151c828d83fa33e5f9c75205517125547
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2025-09-19 19:59:03 +0000

    updating code to trigger redeploy

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { add, subtract } from "./index";
+import { add, subtract, multiply } from "./index";
 import { expect, test } from "vitest";
 
 test("adds 1 + 2 to equal 3", () => {
@@ -7,4 +7,8 @@ test("adds 1 + 2 to equal 3", () => {
 
 test("subtracts 5 - 2 to equal 3", () => {
   expect(subtract(5, 2)).toBe(3);
+});
+
+test("multiplies 3 * 4 to equal 12", () => {
+  expect(multiply(3, 4)).toBe(12);
 });
